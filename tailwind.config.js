6e1671443e22d88a575473e9c46fe4ba6b7f912b
nbansaltech <research.nishantbansal@gmail.com>
2025-11-04 09:12:54 -0800

    Update  tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: { extend: {} },
   plugins: [],
 };
